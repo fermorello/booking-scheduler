0018de85c3598d3682b4152f616a33a5bfca7272
ferminmorello <fermin@mubee.io>
2022-11-25 15:15:33 -0300

    date names by language

---
+++ src/helpers/translate.ts
@@ -0,0 +1,30 @@
+export const translate: { [key: string]: any } = {
+    Mo: {
+      en: 'Mo',
+      es: 'Lu'
+    },
+    Tu: {
+      en: 'Tu',
+      es: 'Ma'
+    },
+    We: {
+      en: 'We',
+      es: 'Mi'
+    },
+    Th: {
+      en: 'Th',
+      es: 'Ju'
+    },
+    Fr: {
+      en: 'Fr',
+      es: 'Vi'
+    },
+    Sa: {
+      en: 'Sa',
+      es: 'Sá'
+    },
+    Su: {
+      en: 'Su',
+      es: 'Do'
+    },
+  };
