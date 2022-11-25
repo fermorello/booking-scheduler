b2ffc8d29c345a438eea5ed2ec240815c97b007c
ferminmorello <fermin@mubee.io>
2022-11-25 17:00:25 -0300

    import of new Configuration type directory

--- src/hooks/useConfiguration.ts
+++ src/hooks/useConfiguration.ts
@@ -1,5 +1,6 @@
 import React from 'react'
-import { configuration, Configuration } from '../App';
+import { configuration } from '../BookingScheduler';
+import { Configuration } from '../types/configuration';
 
 function useConfiguration() {
   const configurationData: Configuration = React.useContext(configuration);
