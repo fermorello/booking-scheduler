2b44d126775680eed24d70327b567922306c4986
ferminmorello <fermin@mubee.io>
2022-11-28 11:48:53 -0300

    exporting all components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as BookingGrid } from './booking-grid';
 export { default as CanvasTable } from './canvas-table';
 export { default as ColumnsComponent } from './columns-component';
 export { default as RowsComponent } from './rows-component';
+export { default as BookingScheduler } from './booking-scheduler';
