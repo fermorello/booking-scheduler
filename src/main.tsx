6b5e4cd225f6785c8f44437f620166d3d05f5d2c
ferminmorello <fermin@mubee.io>
2022-11-28 11:47:12 -0300

    Main file rendering bookingscheduler component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import BookingScheduler from './components/booking-scheduler';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BookingScheduler
+      totalWidth={window.innerWidth}
+      hoursPerDay={1}
+      startRangeDate="2022-11-24"
+      endRangeDate="2022-11-26"
+      onClick={(booking: any) => console.log(booking)}
+      bookingColors={{
+        active: "#00c100db",
+        started: "#00c100db",
+        complete: "#ff5722",
+      }}
+    />
   </React.StrictMode>
 );
