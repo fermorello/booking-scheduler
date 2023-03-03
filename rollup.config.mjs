a6e22df83e1e2b4386d886a2ea9788c277afe9f3
ferminmorello <fermin@mubee.io>
2023-03-03 15:25:08 -0300

    build::Roll-up configuration - WORKING

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,24 +1,30 @@
 import resolve from "@rollup/plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+import { readFile } from "fs/promises";
+const packageJson = JSON.parse(await readFile("./package.json"));
 
 export default [
   {
     input: "src/index.ts",
+    external: ["react", "react-dom"],
     output: [
       {
-        file: "dist/cjs/index.js",
+        file: packageJson.main,
         format: "cjs",
         sourcemap: true,
       },
       {
-        file: "dist/esm/index.js",
+        file: packageJson.module,
         format: "esm",
         sourcemap: true,
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
