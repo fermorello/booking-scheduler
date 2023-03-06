eca02a86ba3ed0ab2c38102cc660d23612a5d100
ferminmorello <fermin@mubee.io>
2023-03-06 15:54:05 -0300

    design-improvements::Delete not used import

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,6 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
 import { readFile } from "fs/promises";
 const packageJson = JSON.parse(await readFile("./package.json"));
 
